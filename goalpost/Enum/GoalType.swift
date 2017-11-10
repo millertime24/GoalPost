d48f84ab7632c46a725a11567a32a7aa4d7e4c42
Andrew <milldog8@hotmail.com>
2017-11-10 14:15:40 -0700

    goalcells tableview

---
+++ goalpost/Enum/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost
+//
+//  Created by Andrew on 11/10/17.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
