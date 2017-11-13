3fdbb90c7319caa539863e61597eb8ca63519890
Andrew <milldog8@hotmail.com>
2017-11-13 10:22:54 -0700

    finishVC, passing data

--- goalpost/Controller/CreateGoalVC.swift
+++ goalpost/Controller/CreateGoalVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreateGoalVC: UIViewController {
+class CreateGoalVC: UIViewController, UITextViewDelegate {
     @IBOutlet weak var goalTextView: UITextView!
     
     @IBOutlet weak var shortTerm: UIButton!
@@ -23,10 +23,15 @@ class CreateGoalVC: UIViewController {
         nextBtn.bindToKeyboard()
         shortTerm.setSelectedColor()
         longTerm.setDeselectedColor()
-
+        goalTextView.delegate = self
     }
     
     @IBAction func nextBtnWasPressed(_ sender: Any) {
+        if goalTextView.text != "" && goalTextView.text != "What is your goal?" {
+            guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else {return}
+            finishGoalVC.initData(description: goalTextView.text!, type: goalType)
+            presentDetail(finishGoalVC)
+        }
         
     }
     
@@ -48,4 +53,8 @@ class CreateGoalVC: UIViewController {
        dismissDetail()
     }
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        goalTextView.text = ""
+        goalTextView.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
 }
